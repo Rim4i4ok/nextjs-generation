cb2597a921ab69c4c1904fd94d5dece1bf859e46
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-18 21:26:50 +0300

    notfound and redirect

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,23 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+  console.log("RE-generating");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: "/no-data", // will redirect to no-data page
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true, // will return 404 page if no data
+    };
+  }
   return {
     props: {
       products: data.products,
