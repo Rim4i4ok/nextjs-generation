f46644160e2a8af6da58f9b4aa770179665ea35c
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-20 23:31:30 +0300

    ssr and getServerSideProps

---
+++ pages/user-profile.js
@@ -0,0 +1,9 @@
+function UserProfilePage(props) {
+  return <h1>{props.username}</h1>;
+}
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  return { props: { username: "Kenobi" } };
+}
